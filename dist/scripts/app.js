ac6d281cdacfe8f4ef5d10a457302939d94472e1
JMwill <787505998@qq.com>
2016-07-05 21:55:21 +0800

    修复加载两次的问题

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -117,7 +117,7 @@ if (!$) {
         };
 
         this.DEFAULT = {
-            timeout: 10000
+            timeout: 1000
         };
         /**********************************/
 
@@ -136,20 +136,24 @@ if (!$) {
 /*eslint-env es6*/
 Page.init = function () {
     document.onreadystatechange = function () {
-        // create colorful background
         if (document.readyState === 'complete') {
+            // create colorful background
             Page.bg = new Page.BgChanger();
             Page.bg.animateBg();
-        }
 
-        // create image loader
+            // create image loader
 
-        var imgUrls = ['/dist/images/default-bg.jpg'];
-        Page.imgLoader = new Page.BgImageLoader('.background');
+            var imgUrls = ['/dist/images/default-bg.jpg'];
+            Page.imgLoader = new Page.BgImageLoader('.background');
 
-        setInterval(function () {
+            // setInterval(() => {
+            //     Page.imgLoader.loadImg(
+            //         imgUrls[parseInt(Math.random() * imgUrls.length)]
+            //     );
+            // }, Page.imgLoader.DEFAULT.timeout);
+
             Page.imgLoader.loadImg(imgUrls[parseInt(Math.random() * imgUrls.length)]);
-        }, Page.imgLoader.DEFAULT.timeout);
+        }
     };
 };
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,28 @@
 /*eslint-env es6*/
 Page.init = function () {
     document.onreadystatechange = function () {
-        // create colorful background
         if (document.readyState === 'complete') {
+            // create colorful background
             Page.bg = new Page.BgChanger();
             Page.bg.animateBg();
-        }
 
-        // create image loader
+            // create image loader
 
-        var imgUrls = [
-            '/dist/images/default-bg.jpg'
-        ];
-        Page.imgLoader = new Page.BgImageLoader('.background');
+            var imgUrls = [
+                '/dist/images/default-bg.jpg'
+            ];
+            Page.imgLoader = new Page.BgImageLoader('.background');
 
-        setInterval(() => {
+            // setInterval(() => {
+            //     Page.imgLoader.loadImg(
+            //         imgUrls[parseInt(Math.random() * imgUrls.length)]
+            //     );
+            // }, Page.imgLoader.DEFAULT.timeout);
+
             Page.imgLoader.loadImg(
                 imgUrls[parseInt(Math.random() * imgUrls.length)]
             );
-        }, Page.imgLoader.DEFAULT.timeout);
+        }
     };
 };
 
